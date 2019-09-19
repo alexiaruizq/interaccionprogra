d01d7cc0cdb7c78d0ea9094700e9747a1bf8bdf4
alexiaruizq <alexiaruiz-@hotmail.com>
2019-09-19 08:49:36 -0700

    Agregar archivos de proyecto.

---
+++ interaccion/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace interaccion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnMiBoton.Click += BtnMiBoton_Click;
+
+        }
+
+        private void BtnMiBoton_Click(object sender, RoutedEventArgs e)
+        {
+            lblAlexiaRuiz.Text = "Ruiz Alexia";
+            lblAlexiaRuiz.FontSize = 29;
+            lblAlexiaRuiz.Foreground = Brushes.BlueViolet;
+        }
+
+        private void BtnsegudnoBoton_Click(object sender, RoutedEventArgs e)
+        {
+            lblAlexiaRuiz.Text = "Millanes Fatima";
+            lblAlexiaRuiz.FontSize = 22;
+            lblAlexiaRuiz.Foreground = Brushes.Black;
+
+
+        }
+
+        private void BtntercerBoton_Click(object sender, RoutedEventArgs e)
+        {
+
+            lblAlexiaRuiz.Text = "Campbell Raul";
+            lblAlexiaRuiz.FontSize = 26;
+            lblAlexiaRuiz.Foreground = Brushes.Purple;
+
+        }
+    }
+}
